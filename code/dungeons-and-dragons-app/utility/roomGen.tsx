ffd87ffa5bda0eeef8400cb2076af3ba668293f4
Adam Sinclair <sinclaadam@ecs.vuw.an.nz>
2021-07-06 06:22:37 +0800

    added documentation to functions, added todos where needed to add clarity

--- code/dungeons-and-dragons-app/utility/roomGen.tsx
+++ code/dungeons-and-dragons-app/utility/roomGen.tsx
@@ -1,3 +1,4 @@
+
 const EMPTY = 0;
 const ROOM_TILE = 1;
 const ENTRANCE = 2;
@@ -9,10 +10,11 @@ const ENTRANCE = 2;
  * @param colSize the number of columns
  * @param entrances the entrances (array of x,y coordinates eg [[1,3],[3,2]])
  * @param growProbability the probability that the room will grow (that is, it will be larger)
+ * @param clean if you want the room to be cleaned of single tiles left after the grow function
  * @return room The new room in array form.
  */
 export function roomGen(rowSize: number, colSize: number, entrances: number[][], growProbability: number, clean: boolean): number[][] {
-    // preconditions: rowsize/colsize must be positive
+    // preconditions: rowSize/colSize must be positive
     // grow probability(x) = 0 > x <= 1
     // at least one entrance
     // entrances are within the row/col size
@@ -20,18 +22,22 @@ export function roomGen(rowSize: number, colSize: number, entrances: number[][],
 
     const singleEntrance = entrances.length == 1;
 
+    //todo handle single entrances with simple/modified grow function
+
     // Start with array of 0's
     let room = Array.from(Array(rowSize), _ => Array(colSize).fill(EMPTY));
 
 
     // assign entrances
-
     for (let i = 0; i < entrances.length; i++) {
         let row = entrances[i][0];
         let col = entrances[i][1];
 
         room[row][col] = ENTRANCE;
     }
+
+    //todo we could grow horizontally instead of vertically for more variation if we wanted
+    // this would involve using the middleY value instead of the middleX value.
 
     // figure out middle row and col if entrances > 1
     let middleX = 0;
@@ -74,7 +80,7 @@ export function roomGen(rowSize: number, colSize: number, entrances: number[][],
         colMax = Math.max(colMax, entranceCol);
     }
 
-    // grow cols to connect
+    // grow cols to connect all entrances
     while (colMin < colMax) {
         colMin++;
         room[middleX][colMin] = ROOM_TILE;
@@ -109,6 +115,17 @@ export function roomGen(rowSize: number, colSize: number, entrances: number[][],
         spread(row, col - 1); // left
     }
 
+    if (clean) cleanFinish();
+
+    return room;
+
+    /**
+     * Recursively spreads the room tile at row,col to the surrounding empty tiles with a probability given into the
+     * roomGen function.
+     *
+     * @param row the row to spread from
+     * @param col the col to spread from
+     */
     function spread(row: number, col: number) {
         // check bounds/conditions
         if (row >= 0 && row < room.length && col >= 0 && col < room[row].length && room[row][col] == EMPTY) {
@@ -124,6 +141,10 @@ export function roomGen(rowSize: number, colSize: number, entrances: number[][],
         }
     }
 
+    /**
+     * Looks at each tile in the final map to remove any single empty tiles with all adjacent room tiles to make the
+     * map look cleaner.
+     */
     function cleanFinish() {
         for (let row = 1; row < room.length - 1; row++) {
             for (let col = 1; col < room[row].length - 1; col++) {
@@ -139,8 +160,4 @@ export function roomGen(rowSize: number, colSize: number, entrances: number[][],
             }
         }
     }
-
-    if (clean) cleanFinish();
-
-    return room;
 }
