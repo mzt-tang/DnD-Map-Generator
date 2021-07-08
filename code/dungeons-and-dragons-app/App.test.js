73ba88717993db6817fd123f64d5b203b4d96913
Destiro <debruconn@ecs.vuw.ac.nz>
2021-07-09 11:55:51 +1200

    Added 2 example tests

--- code/dungeons-and-dragons-app/App.test.js
+++ code/dungeons-and-dragons-app/App.test.js
@@ -7,8 +7,15 @@ import renderer from 'react-test-renderer';
 import App from './App';
 
 describe('<App />', () => {
-    it('has 1 child', () => {
+    //This is an example of a test
+    /*it('has 1 child', () => {
         const tree = renderer.create(<App />).toJSON();
         expect(tree.children.length).toBe(1);
-    });
+    });*/
+
+    //This is an example of a snapshot test (states of UI)
+    /*it('renders correctly', () => {
+        const tree = renderer.create(<App />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });*/
 });
