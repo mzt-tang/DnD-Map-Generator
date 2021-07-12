62d51743c4c098d5c73d04636dcae4b9ae201d7b
Adam Sinclair <50856879+sinclaadam@users.noreply.github.com>
2021-07-13 10:40:13 +1200

    extract room generation method to allow for more than one generation method to be used

--- code/dungeons-and-dragons-app/utility/roomGen.tsx
+++ code/dungeons-and-dragons-app/utility/roomGen.tsx
@@ -22,8 +22,6 @@ export function roomGen(rowSize: number, colSize: number, entrances: number[][],
 
     const singleEntrance = entrances.length == 1;
 
-    //todo handle single entrances with simple/modified grow function
-
     // Start with array of 0's
     let room = Array.from(Array(rowSize), _ => Array(colSize).fill(EMPTY));
 
@@ -40,97 +38,100 @@ export function roomGen(rowSize: number, colSize: number, entrances: number[][],
         room[row][col] = ENTRANCE;
     }
 
-    /** we could grow horizontally instead of vertically for more variation if we wanted
-     this would involve using the middleY value instead of the middleX value. */
-
-    // figure out middle row and col if entrances > 1
-    let middleX = 0;
-    let middleY = 0;
-
-    for (let i = 0; i < entrances.length; i++) {
-        middleX += entrances[i][0];
-        middleY += entrances[i][1];
-    }
-
-    // we round to the nearest int using math.floor. This ensures it is within scope.
-    middleX = Math.floor(middleX / entrances.length);
-    middleY = Math.floor(middleY / entrances.length);
-
-    // first move rows towards middle row
-    for (let i = 0; i < entrances.length; i++) {
-        let entrance = entrances[i];
-        let entranceRow = entrance[0];
-        let entranceCol = entrance[1];
-
-        //Checks if entrance is on column edge. If it is move it 1 over to avoid a edge path.
-        if (entranceCol == 0) {
-            entranceCol++;
-            room[entranceRow][entranceCol] = ROOM_TILE;
-        } else if (entranceCol == room[0].length-1) {
-            entranceCol--;
-            room[entranceRow][entranceCol] = ROOM_TILE;
-        }
-
-        // grow towards middle, whichever way that is.
-        while (entranceRow < middleX) {
-            entranceRow++;
-            room[entranceRow][entranceCol] = ROOM_TILE;
-        }
-        while (entranceRow > middleX) {
-            entranceRow--;
-            room[entranceRow][entranceCol] = ROOM_TILE
-        }
-    }
-
-    // ensure middle cols grow to size of rows bounds.
-    let colMin = colSize-1;
-    let colMax = 0;
-
-    for (let i = 0; i < entrances.length; i++) {
-        let entrance = entrances[i];
-        let entranceCol = entrance[1];
-        colMin = Math.min(colMin, entranceCol);
-        colMax = Math.max(colMax, entranceCol);
-    }
-
-    // grow cols to connect all entrances
-    while (colMin < colMax-1) {
-        colMin++;
-        room[middleX][colMin] = ROOM_TILE;
-    }
-
-    // entrances should be connected, grow tiles with probability depending on surrounding tiles.
-
-    // find all tiles that have an adjacent floor tiles. The more floor tiles the higher change to become a floor tile.
-
-    // array of x,y coords where empty tiles are.
-
-
-    const roomTiles: number[][] = [];
-
-    // collect all room tiles
-    for (let i = 0; i < room.length; i++) {
-        for (let j = 0; j < room[i].length; j++) {
-            if (room[i][j] == ROOM_TILE) {
-                roomTiles.push([i, j]);
-            }
-        }
-    }
-
-    // spread from room tiles to empty tiles
-    for (let i = 0; i < roomTiles.length; i++) {
-        const row = roomTiles[i][0];
-        const col = roomTiles[i][1];
-
-        spread(row - 1, col);  // down
-        spread(row, col + 1);  // right
-        spread(row + 1, col); // up
-        spread(row, col - 1); // left
-    }
-
-    if (clean) cleanFinish();
+    growRoomInwards();
+    spreadRoomTiles();
 
     return room;
+
+    /**
+     * Collects all the room tiles which connect the entrances and spreads with the probability passed to the room.
+     */
+    function spreadRoomTiles() {
+        const roomTiles: number[][] = [];
+
+        // collect all room tiles
+        for (let i = 0; i < room.length; i++) {
+            for (let j = 0; j < room[i].length; j++) {
+                if (room[i][j] == ROOM_TILE) {
+                    roomTiles.push([i, j]);
+                }
+            }
+        }
+
+        // spread from room tiles to empty tiles
+        for (let i = 0; i < roomTiles.length; i++) {
+            const row = roomTiles[i][0];
+            const col = roomTiles[i][1];
+
+            spread(row - 1, col);  // down
+            spread(row, col + 1);  // right
+            spread(row + 1, col); // up
+            spread(row, col - 1); // left
+        }
+
+        if (clean) cleanFinish();
+    }
+
+    /**
+     * This specific function is for multiple entrances and grows inwards towards the middle to connect the entrances.
+     */
+    function growRoomInwards() {
+        // figure out middle row and col if entrances > 1
+        let middleX = 0;
+        let middleY = 0;
+
+        for (let i = 0; i < entrances.length; i++) {
+            middleX += entrances[i][0];
+            middleY += entrances[i][1];
+        }
+
+        // we round to the nearest int using math.floor. This ensures it is within scope.
+        middleX = Math.floor(middleX / entrances.length);
+        middleY = Math.floor(middleY / entrances.length);
+
+        // first move rows towards middle row
+        for (let i = 0; i < entrances.length; i++) {
+            let entrance = entrances[i];
+            let entranceRow = entrance[0];
+            let entranceCol = entrance[1];
+
+            //Checks if entrance is on column edge. If it is move it 1 over to avoid a edge path.
+            if (entranceCol == 0) {
+                entranceCol++;
+                room[entranceRow][entranceCol] = ROOM_TILE;
+            } else if (entranceCol == room[0].length - 1) {
+                entranceCol--;
+                room[entranceRow][entranceCol] = ROOM_TILE;
+            }
+
+            // grow towards middle, whichever way that is.
+            while (entranceRow < middleX) {
+                entranceRow++;
+                room[entranceRow][entranceCol] = ROOM_TILE;
+            }
+            while (entranceRow > middleX) {
+                entranceRow--;
+                room[entranceRow][entranceCol] = ROOM_TILE
+            }
+        }
+
+        // ensure middle cols grow to size of rows bounds.
+        let colMin = colSize - 1;
+        let colMax = 0;
+
+        for (let i = 0; i < entrances.length; i++) {
+            let entrance = entrances[i];
+            let entranceCol = entrance[1];
+            colMin = Math.min(colMin, entranceCol);
+            colMax = Math.max(colMax, entranceCol);
+        }
+
+        // grow cols to connect all entrances
+        while (colMin < colMax - 1) {
+            colMin++;
+            room[middleX][colMin] = ROOM_TILE;
+        }
+    }
 
     /**
      * Recursively spreads the room tile at row,col to the surrounding empty tiles with a probability given into the
