6f52da5540e49b2a8998ca1129c55b32fa081140
Adam Sinclair <50856879+sinclaadam@users.noreply.github.com>
2021-07-23 11:12:36 +1200

    fix room generation to use correct grid other

--- code/dungeons-and-dragons-app/pages/Room.tsx
+++ code/dungeons-and-dragons-app/pages/Room.tsx
@@ -15,7 +15,7 @@ import Image8 from '../assets/Images2/RightWall.png';
 import Image9 from '../assets/Images2/Floor.png';
 
 // Import styles
-import GridOther from "../components/GridOther1";
+import GridOther from "../components/GridOther";
 import '../styles/style.css';
 import {roomGen} from "../utility/roomGen";
 
@@ -23,30 +23,18 @@ const Room = () => {
     const[rows, setRows] = useState(20);
     const[cols,setCols] = useState(20);
     const[probability,setProbability] = useState(0.35);
-    const[entrances,setEntrances] = useState([[10,0],[0,12],[19,19]])
+    const[entrances,setEntrances] = useState([[12,19],[0,12],[18,0]])
     const[clean, setClean] = useState(true)
 
     const images : JSX.Element[] = [
         <img className="grid_img" src={empty}/>,    // empty tile
         <img className="grid_img" src={floor}/>,    // floor tile
         <img className="grid_img" src={entrance}/>, // entrance tile
-        <img className="grid_img" src={Image1}/>, // top left corner tile
-        <img className="grid_img" src={Image2}/>, // bottom left corner tile
-        <img className="grid_img" src={Image3}/>, // bottom right corner tile
-        <img className="grid_img" src={Image4}/>, // top right corner tile
-        <img className="grid_img" src={Image5}/>, // bottom wall tile
-        <img className="grid_img" src={Image6}/>, // top wall tile
-        <img className="grid_img" src={Image7}/>, // left wall tile
-        <img className="grid_img" src={Image8}/>, // right wall tile
-        <img className="grid_img" src={Image9}/>, // another floor tile
     ]
 
-    // const entrances = [[10, 0], [0, 12], [19, 19]];
-    // const rows = 20;
-    // const cols = 20;
-    // let prob = 0.5
-
     const tiles = roomGen(rows,cols,entrances,probability,clean);
+
+    console.log(tiles);
 
     const updateProbability = (event: any, newValue: number | number[]) => {
         setProbability(newValue as number);
