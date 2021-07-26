4a40e178275de4e27fbe26e1290247ff27998824
Adam Sinclair <50856879+sinclaadam@users.noreply.github.com>
2021-07-27 09:52:13 +1200

    Implement player view button, DM button and routes

--- code/dungeons-and-dragons-app/pages/Home.tsx
+++ code/dungeons-and-dragons-app/pages/Home.tsx
@@ -1,34 +1,19 @@
-import { Button, Typography } from '@material-ui/core';
 import React, { useState } from 'react';
-import { View } from 'react-native';
-import {Link } from "react-router-dom";
-import CustomAppBar from '../Components/CustomAppBar';
-import ContactButton from '../Components/ContactButton';
+import {View} from "react-native";
+import LargeButton from "../components/LargeButton";
+
 
 const Home = () => {
 
-    // var count : number = 0; // Even though this is a mutable variable, the return will not recognise this.
-    const [count, setCount] = useState(0); //This is a hook, allowing for dynamic changes to the display
 
     return (
         <View>
             <div>
-                <p>
-                    Click buttons below to visit current pages.
-                    <br/>
-
-                </p>
-                <Link to="/route"><button>
-                    Go to Route Example
-                </button>
-                </Link>
-                <Link to="/room"><button>
-                    Go to Room Gen Example
-                </button>
-                </Link>
+                <LargeButton buttonString={"DM View"} buttonRoute={"/dm"}/>
+                <LargeButton buttonString={"Player View"} buttonRoute={"/player"}/>
             </div>
-            <br/>
         </View>
+
     );
 }
 
