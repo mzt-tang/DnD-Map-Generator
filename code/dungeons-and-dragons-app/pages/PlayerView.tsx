71f51051668d3b800cdee2b45d43f8104366d75d
Adam Sinclair <50856879+sinclaadam@users.noreply.github.com>
2021-07-27 11:27:58 +1200

    playerview connects to firebase and retrives map and tile size values

--- code/dungeons-and-dragons-app/pages/PlayerView.tsx
+++ code/dungeons-and-dragons-app/pages/PlayerView.tsx
@@ -1,7 +1,34 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import { View } from 'react-native';
+import { db } from "../firebaseConfig";
+import firebase from 'firebase';
+
 
 const PlayerView = () => {
+
+    const[map, setMap] = useState([]);
+    const[size, setSize] = useState(25); // set the tile size?
+
+    //todo figure out a better way to init firebase
+    const database = db;
+
+
+    // Get map from firebase.
+
+    useEffect(() => {
+        //Connect to Real Time Database
+        const playerViewDatabase = firebase.database().ref().child('playerView');
+        const tileSize = firebase.database().ref().child('tileSize');
+
+        // Update view
+        playerViewDatabase.on('value', map => console.log(map.val()));
+        tileSize.on('value', size => console.log(size.val()));
+
+    }, []);
+
+
+    //todo return based on map.. return loading screen if map empty, otherwise display map.
+
     return (
         <View>
         <h1>
