2275ed3eb5c994d784515aa307bb6e21e6d79258
Adam Sinclair <50856879+sinclaadam@users.noreply.github.com>
2021-07-27 09:23:50 +1200

    added basics to playerview and dm pages

---
+++ code/dungeons-and-dragons-app/pages/DmView.tsx
@@ -0,0 +1,13 @@
+import React, {ChangeEvent, useState} from 'react';
+import { View } from 'react-native';
+
+const DmView = () => {
+    return (
+        <View>
+            <h1>
+                This is the DM view
+            </h1>
+        </View>
+    );
+}
+export default DmView
---
+++ code/dungeons-and-dragons-app/pages/PlayerView.tsx
@@ -0,0 +1,13 @@
+import React, {ChangeEvent, useState} from 'react';
+import { View } from 'react-native';
+
+const PlayerView = () => {
+    return (
+        <View>
+        <h1>
+            This is the player view
+        </h1>
+        </View>
+    );
+}
+export default PlayerView
