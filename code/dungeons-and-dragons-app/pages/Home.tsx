e423e12d89de01d0429868516907ffb90ce7c320
Adam Sinclair <50856879+sinclaadam@users.noreply.github.com>
2021-07-27 10:17:47 +1200

    centre player and dm buttons on home page

--- code/dungeons-and-dragons-app/pages/Home.tsx
+++ code/dungeons-and-dragons-app/pages/Home.tsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import {View} from "react-native";
 import LargeButton from "../components/LargeButton";
+import '../styles/style.css'
 
 
 const Home = () => {
 
 
+
     return (
         <View>
-            <div>
+            <div className={"container"}>
+            <div className={"homeButtons"}>
                 <LargeButton buttonString={"DM View"} buttonRoute={"/dm"}/>
                 <LargeButton buttonString={"Player View"} buttonRoute={"/player"}/>
+            </div>
             </div>
         </View>
 
