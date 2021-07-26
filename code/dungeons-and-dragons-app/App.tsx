af0c9b40543262c99b0b41c7ed2d06a4771f61cd
Adam Sinclair <50856879+sinclaadam@users.noreply.github.com>
2021-07-27 09:25:35 +1200

    setup routing for new pages

--- code/dungeons-and-dragons-app/App.tsx
+++ code/dungeons-and-dragons-app/App.tsx
@@ -4,14 +4,18 @@ import React from 'react';
 import Home from "./pages/Home";
 import RouteFindingMap from "./pages/RouteFindingMap";
 import Room from "./pages/Room";
+import PlayerView from "./pages/PlayerView"
+import DmView from "./pages/DmView";
 
 export default function App() {
   return (
       <BrowserRouter>
         <Route path = "/home" component = {Home} />
-          <Route path = "/" component = {Home} />
+          <Route path = "/" exact={true} component = {Home} />
           <Route path = "/route" component = {RouteFindingMap} />
           <Route path = "/room" component = {Room} />
+          <Route path = "/player" component={PlayerView}/>
+          <Route path = "/dm" component={DmView}/>
       </BrowserRouter>
   );
 }
