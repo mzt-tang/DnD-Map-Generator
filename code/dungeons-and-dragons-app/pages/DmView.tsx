aeaeb8a1f7c3afaf09480ca08e9cb51dfd462450
Michael Tang <tangmich1@ecs.vuw.ac.nz>
2021-07-27 12:59:39 +1200

    Testing material UI in DmView

--- code/dungeons-and-dragons-app/pages/DmView.tsx
+++ code/dungeons-and-dragons-app/pages/DmView.tsx
@@ -1,13 +1,33 @@
 import React, {ChangeEvent, useState} from 'react';
 import { View } from 'react-native';
+import {makeStyles, Paper} from "@material-ui/core";
+import {AppBar, Grid, IconButton, Link, Toolbar, Typography} from "@material-ui/core";
+import {Title} from "@material-ui/icons";
 
-const DmView = () => {
+const useStyles = makeStyles((theme) => ({
+    grid: {
+        width: '100%',
+        margin: '0px'
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        background: theme.palette.success.light,
+    }
+}));
+
+function DmView() {
+    const classes = useStyles();
+
     return (
-        <View>
-            <h1>
-                This is the DM view
-            </h1>
-        </View>
-    );
+        <Grid container spacing={2} className={classes.grid}>
+            <Grid item xs={6}>
+                <Paper className={classes.paper}>1</Paper>
+            </Grid>
+        </Grid>
+    )
 }
+
+
 export default DmView
