7c877ef4b64ef3821ae2a98b8438acea8bc560e7
Elijah Guarina <guarinelij@myvuw.ac.nz>
2021-08-10 10:32:03 +1200

    Added SmallBackButton component

---
+++ code/dungeons-and-dragons-app/components/SmallBackButton.tsx
@@ -0,0 +1,56 @@
+import {Button, Typography} from '@material-ui/core';
+import React from 'react';
+import { useHistory } from "react-router-dom";
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+
+interface Props {
+    buttonString : string
+    buttonRoute : string
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        smallBackButton: {
+            color: '#Eddded',
+            background: '#000000',
+            borderColor: '#Eddded',
+            borderWidth: 2,
+            opacity: 0.8,
+            margin: 25,
+            padding: 20,
+            alignSelf: 'center'
+
+        },
+        buttonText: {
+            fontSize: 16,
+            color: '#Eddded'
+        },
+    }),
+);
+
+const SmallMenuButton = (props : Props) => {
+    const classes = useStyles();
+
+    // Props Initialisations
+    const buttonString : string = props.buttonString;
+    const buttonRoute : string = props.buttonRoute;
+
+    //Other Variable Initialisations
+    const history = useHistory();
+
+    return (
+        <div className={"SmallMenuButton"}>
+        <Button variant="outlined" size="small" color="primary" startIcon={<ArrowBackIosIcon/>} className={classes.smallBackButton} onClick={() => {
+            history.push(buttonRoute)
+        }}>
+
+            <Typography variant={"button"} className={classes.buttonText} >
+                {buttonString}
+            </Typography>
+        </Button>
+        </div>
+    );
+}
+
+export default SmallMenuButton
