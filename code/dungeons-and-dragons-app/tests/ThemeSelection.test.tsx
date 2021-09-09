044c4247a51678d679db419af35fe141cd5d630c
Ollie Badrick <49539730+badricoliv@users.noreply.github.com>
2021-09-10 10:40:44 +1200

    snapshot test for theme selection

---
+++ code/dungeons-and-dragons-app/tests/ThemeSelection.test.tsx
@@ -0,0 +1,18 @@
+// This is template code for testing using jest for the CI part of CI/CD
+// The guide im following is: https://docs.expo.io/guides/testing-with-jest/
+
+// To install the jest test library: "yarn add jest-expo" or "npm install jest-expo"
+// To run all tests: "npx jest --ci"
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ThemeSelect  from '../components/ThemeSelect';
+
+
+describe("snaphot testing", () => {
+    test("snapshot for theme selection", () => {
+        function onChange() {}
+        const renderedComponent = renderer.create(<ThemeSelect themeList={["Caves", "Dungeon", "Underground Mansion"]} theme={"Caves"} onChange={onChange}/>).toJSON();
+        expect(renderedComponent).toMatchSnapshot();
+    })
+})
