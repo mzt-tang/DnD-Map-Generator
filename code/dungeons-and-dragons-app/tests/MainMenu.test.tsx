e3b974d35fecc473980b4a60f96fc6f707544369
Connor de Bruyn <debruconn@ecs.vuw.ac.nz>
2021-09-10 09:32:51 +1200

    Added empty main menu test file

---
+++ code/dungeons-and-dragons-app/tests/MainMenu.test.tsx
@@ -0,0 +1,16 @@
+// This is template code for testing using jest for the CI part of CI/CD
+// The guide im following is: https://docs.expo.io/guides/testing-with-jest/
+
+// To install the jest test library: "yarn add jest-expo" or "npm install jest-expo"
+// To run all tests: "npx jest --ci"
+
+import React from 'react';
+
+import './RoomGeneration.test';
+
+describe('<MainMenu />', () => {
+    /**
+     * TESTING FRONT END COMPONENTS IN MAIN MENU SECTION
+     */
+
+});
