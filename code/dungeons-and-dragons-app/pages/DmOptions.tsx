22acd98a1cd62fa2f0320b052d5060538aaab84e
Adam Sinclair <sinclaadam@vuw.ac.nz>
2021-10-05 10:28:29 +1300

    refactor code to state, implement activity indicator

--- code/dungeons-and-dragons-app/pages/DmOptions.tsx
+++ code/dungeons-and-dragons-app/pages/DmOptions.tsx
@@ -1,6 +1,6 @@
-import { View, Pressable, Text } from "react-native";
-import React, { useState } from "react";
-import { Typography, Radio } from "@material-ui/core";
+import {View, Pressable, Text, ActivityIndicator} from "react-native";
+import React, {useState} from "react";
+import {Typography, Radio} from "@material-ui/core";
 import ThemeSelect from "../components/ThemeSelect";
 import CodeInput from "../components/CodeInput";
 import SmallMenuButton from "../components/SmallMenuButton";
@@ -10,26 +10,19 @@ import GenRandomCode from "../utility/GenRandomCode";
 
 const DmOptions = () => {
     const [theme, setTheme] = useState("Caves");
-    console.log(theme)
+    const [code, setCode] = useState('')
+    const [loading, setLoading] = useState(false);
 
-    let code : string = "";
+    const activityIndicator = <ActivityIndicator size={60} color="#00ff00" style={{padding: 10}}/>;
 
-    function handleCodeChange(newcode : string){
-        code = newcode;
-    }
-
-    function fetchCode() : string {
-        return code;
-    }
-
-    function genRandomCode() : string {
+    function genRandomCode(): string {
         return GenRandomCode()
     }
 
     return (
         <View>
-            <div className={"backgroundImage"} >
-                <div className={"dmOptionBox"} >
+            <div className={"backgroundImage"}>
+                <div className={"dmOptionBox"}>
                     <Typography variant={"h2"} className={"dmTitle"}>
                         Dungeon Master Options
                     </Typography>
@@ -37,10 +30,13 @@ const DmOptions = () => {
                         <Typography variant={"h4"} className={"dmSubtitle"}>
                             Load Existing Game
                         </Typography>
-                        <CodeInput defaultText={""} labelText={"Enter a Game Code Here"} onCodeChange={handleCodeChange} />
+                        <CodeInput defaultText={""} labelText={"Enter a Game Code Here"} onCodeChange={setCode}/>
+                        {loading ? activityIndicator : <View/>}
                         <div className={"loadGameButtons"}>
-                            <SmallBackButton buttonString={"Back"} buttonRoute={""} />
-                            <SmallMenuButton buttonString={"Load Game"} buttonRoute={"/dm"} code={fetchCode} buttonProp={""} creatingNewGame={false}/>
+                            <SmallBackButton buttonString={"Back"} buttonRoute={""}/>
+                            <SmallMenuButton buttonString={"Load Game"} buttonRoute={"/dm"} code={() => code}
+                                             buttonProp={""} creatingNewGame={false} disabled={loading}
+                                             onclick={setLoading}/>
                         </div>
                     </div>
                     <div className={"dmOptionRight"}>
@@ -48,11 +44,13 @@ const DmOptions = () => {
                             New Game
                         </Typography>
 
-                        <ThemeSelect themeList={["Caves", "Dungeon", "Underground Mansion"]} onChange={setTheme} theme={theme} />
+                        <ThemeSelect themeList={["Caves", "Dungeon", "Underground Mansion"]} onChange={setTheme}
+                                     theme={theme}/>
                         <div className={"createButton"}>
-                            <SmallMenuButton buttonString={"Create"} buttonRoute={"/dm"} code={genRandomCode} buttonProp={theme} creatingNewGame={true}/>
+                            <SmallMenuButton buttonString={"Create"} buttonRoute={"/dm"} code={genRandomCode}
+                                             buttonProp={theme} creatingNewGame={true}/>
                         </div>
-                        
+
 
                     </div>
                 </div>
